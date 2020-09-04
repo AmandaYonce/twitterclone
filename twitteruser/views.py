72605cc57209c8d2085163759bb564b620d6ecc4
Amanda Yonce <amanda.stork@gmail.com>
2020-09-04 16:15:48 -0400

    cleaned up UI

--- twitteruser/views.py
+++ twitteruser/views.py
@@ -1,11 +1,9 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from .forms import CustomUserCreationForm
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from tweet.models import Tweet
 from .models import *
-from django.db.models import Q
-from django.contrib.auth.decorators import login_required
 from notification.models import Notification
 
 
@@ -15,20 +13,21 @@ class SignUpView(CreateView):
     template_name = 'signup.html'
 
 
-@login_required
 def Home(request):
-    author = CustomUser.objects.get(id=request.user.id)
-    myfollowers = author.followers.all()
-    followingTweets=[]
-    mytweets = Tweet.objects.filter(author=request.user)
-    if mytweets:
-        for myeach in mytweets:
-            followingTweets.append(myeach)
-    for each in myfollowers:
-        ftweets = Tweet.objects.filter(author=each)
-        if len(ftweets) > 0:
-            for feach in ftweets:
-                followingTweets.append(feach)
-    followingTweets = sorted(followingTweets, key=lambda k: (k.timestamp))
-    count_notif = Notification.objects.filter(at_person=request.user.id, read=False).count()
-    return render(request, 'home.html', {'followingTweets': followingTweets, 'count_notif': count_notif})
+    if request.user.is_authenticated: 
+        author = CustomUser.objects.get(id=request.user.id)
+        myfollowers = author.followers.all()
+        followingTweets=[]
+        mytweets = Tweet.objects.filter(author=request.user)
+        if mytweets:
+            for myeach in mytweets:
+                followingTweets.append(myeach)
+        for each in myfollowers:
+            ftweets = Tweet.objects.filter(author=each)
+            if len(ftweets) > 0:
+                for feach in ftweets:
+                    followingTweets.append(feach)
+        followingTweets = sorted(followingTweets, key=lambda k: (k.timestamp))
+        count_notif = Notification.objects.filter(at_person=request.user.id, read=False).count()
+        return render(request, 'home.html', {'followingTweets': followingTweets, 'count_notif': count_notif})
+    return HttpResponseRedirect("/users/login/")
